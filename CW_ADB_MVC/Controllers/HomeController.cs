0b8df5520958ac1e5ac10678baaf98dd663981ca
olgasn <olgasn@inbox.ru>
2015-03-10 12:43:09 +0300

    Добавлена модель для хранения ссылки на текущую операцию

--- CW_ADB_MVC/Controllers/HomeController.cs
+++ CW_ADB_MVC/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace CW_ADB_MVC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Описание";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Контакты";
 
             return View();
         }
---
+++ CW_ADB_MVC/Models/CurrentOperationModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CW_ADB_MVC.Models
+{
+    public class CurrentOperationModel
+    {
+        public int CurrentOperationID { get; set; }
+        public int CurrentFuelID { get; set; }
+        public int CurrentTankID { get; set; }
+        public string CurrentFuelType { get; set; }
+        public string CurrentTankType { get; set; }
+        public int CurrentPage { get; set; }
+
+    }
+}
