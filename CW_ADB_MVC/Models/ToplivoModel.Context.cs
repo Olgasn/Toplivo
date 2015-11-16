a1c81bcf51edd791393b80c4282dbfece678b407
olgasn <olgasn@inbox.ru>
2015-11-16 23:37:48 +0300

    OnModelCreating

--- CW_ADB_MVC/Models/ToplivoModel.Context.cs
+++ CW_ADB_MVC/Models/ToplivoModel.Context.cs
@@ -22,7 +22,7 @@ namespace CW_ADB_MVC.Models
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            throw new UnintentionalCodeFirstException();
+            //throw new UnintentionalCodeFirstException();
         }
     
         public virtual DbSet<Fuels> Fuels { get; set; }
